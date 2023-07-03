1a3af00a829175f7c5c4cab8a5edde36257a4c0e
GabrielaVelasco <gabriela.luppe@gmail.com>
2023-07-03 09:47:25 +0200

    user.service.ts is logged in

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -12,11 +13,17 @@ export class UserService {
     isLoggedOut$: Observable<boolean>;
     pictureUrl$: Observable<string>;
 
-    constructor(private aufAuth: AngularFireAuth ) {
+    constructor(private afAuth: AngularFireAuth ) {
         //Just to show how it works
-        // aufAuth.idToken.subscribe(jwt => console.log("jwt", jwt));
-        // aufAuth.authState.subscribe(auth => console.log("auth", auth));
+        // afAuth.idToken.subscribe(jwt => console.log("jwt", jwt));
+        // afAuth.authState.subscribe(auth => console.log("auth", auth));
+
+        this.isLoggedIn$ = afAuth.authState.pipe(map(user => !!user));
+        this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+        this.pictureUrl$ = afAuth.authState.pipe(map(user => user? user.photoURL : null));
     }
+
+    
     
 }
 
