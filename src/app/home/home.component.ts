5689df875409c4e7326ba9a7c4202b1bd1eafd08
GabrielaVelasco <gabriela.luppe@gmail.com>
2023-07-11 14:41:59 +0200

    till lesson 43 -showing - hiding features

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,33 +5,33 @@ import {catchError, map} from 'rxjs/operators';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Router} from '@angular/router';
 import { CoursesService } from '../services/courses.service';
+import { UserService } from '../services/user.service';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  beginnersCourses$: Observable<Course[]>;
 
-    beginnersCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
-    advancedCourses$: Observable<Course[]>;
+  constructor(
+    private router: Router,
+    private coursesService: CoursesService,
+    public user: UserService
+  ) {}
 
-    constructor(
-      private router: Router,
-      private coursesService: CoursesService) {}
+  ngOnInit() {
+    this.reloadCourses();
+  }
 
-    ngOnInit() {
-
-      this.reloadCourses();
-
-    }
-
-    reloadCourses() {
-
-      this.beginnersCourses$ = this.coursesService.loadCoursesByCategory("BEGINNER");
-      this.advancedCourses$ = this.coursesService.loadCoursesByCategory("ADVANCED");
-    }
-
+  reloadCourses() {
+    this.beginnersCourses$ =
+      this.coursesService.loadCoursesByCategory("BEGINNER");
+    this.advancedCourses$ =
+      this.coursesService.loadCoursesByCategory("ADVANCED");
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from "@angular/fire/auth";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { UserRoles } from "../model/user-roles";
 
 @Injectable({
     providedIn: 'root'
@@ -13,6 +14,7 @@ export class UserService {
     isLoggedIn$: Observable<boolean>;
     isLoggedOut$: Observable<boolean>;
     pictureUrl$: Observable<string>;
+    roles$: Observable<UserRoles>;
 
     constructor(
         private afAuth: AngularFireAuth,
@@ -24,6 +26,9 @@ export class UserService {
         this.isLoggedIn$ = afAuth.authState.pipe(map(user => !!user));
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
         this.pictureUrl$ = afAuth.authState.pipe(map(user => user? user.photoURL : null));
+        this.roles$ = this.afAuth.idTokenResult.pipe(
+          map((token) => <any>token?.claims ?? { admin: false })
+        ); 
     }
 
     logout() {
